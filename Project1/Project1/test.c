989617f71d0a2dd2f76654d10e5cdf18023ee28a
muyan6 <45909799+muyan6@users.noreply.github.com>
2021-09-15 11:21:41 +0800

    Add files via upload

---
+++ Project1/Project1/test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int people = 100;
+//		people = people + 1
+//		; printf("%d", people);
+//		return 0;
+//}
+//{
+//	int a = 0;
+//	int b = 1;
+//	int sum = 0;
+//		scanf("%d %d", &a,& b);
+//		sum = a + b;
+//			printf("%d",sum);
+//			return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 6;
+//	int sum = 0;
+//	
+//		scanf("%d %d", & a, & b);
+//		sum = a * b;
+//		printf("%d", sum);
+//		return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int d= 0;
+//	int s = 10;
+//	const int h = 4;
+//	scanf("%d %d", & a, &d);
+//	s = a + d;
+//	printf("%d", s);
+//
+//
+//
+//	return 0;
+//}
+//enum sb
+//{
+//	dsb,
+//	xsb,
+//	sssb
+//};
+//int main()
+//{
+//	printf("%d\n", dsb);
+//	printf("%d\n", xsb);
+//	printf("%d", sssb);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int n = 5;
+//	scanf("%d %d", &a, &b);
+//	n = a + b;
+//	printf("%d", n);
+//		return 0;
+//}
+enum age
+{
+	san,
+	er,
+	yi,
+};
+int main()
+{
+	char arr[] = { 'a','b' ,'\0'};
+	char arr1[] = "abc";
+	printf("%s\n", arr1);
+		
+	printf("%s", arr);
+	return 0;
+}
